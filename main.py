8a7e0d4bff8a47d2ef1f802bab06690914631682
mbicanic <miro.bicanic@gmail.com>
2021-05-22 03:48:07 +0200

    Refactor main to work with new extraction

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pprint
 
 import numpy as np
 
+import classifier
+from extractor import *
 from baselines import RandomBaseline, MCCBaseline
-from dataset import load_dataset, split_dataset, TRAITS
+from dataset import load_features, TRAITS
 from eval import eval
 from utils import setup_torch_reproducibility, setup_torch_device, project_path, get_str_formatted_time, ensure_dir
 
@@ -14,32 +16,39 @@ if __name__ == '__main__':
     print("FER, University of Zagreb, Croatia")
 
     # ~~~~ Config ~~~~ #
-    n_runs = 10  # How many times will each model be run, used to calculate std. dev.
+    n_runs = 1  # How many times will each model be run, used to calculate std. dev.
     seed = 72  # Initial seed, used to setup reproducibility
     time_str = get_str_formatted_time()  # string time identifier
-    test_split_ratio, val_split_ratio = 0.2, 0
+    test_split_ratio, val_split_ratio = 0.2, 0.2
     run_name = f"baselines_s={seed}_n={n_runs}_testratio={test_split_ratio}"  # The run name is used for logging
     save_dir = os.path.join(project_path, f"imgs/{run_name}")  # Where should logs be saved
     ensure_dir(save_dir)
-    classifiers = []
-    classifiers += [("MCCBaseline", MCCBaseline())]
-    classifiers += [("RandomBaseline", RandomBaseline())]
 
     # ~~~~ Setup ~~~~ #
     device = setup_torch_device()
     setup_torch_reproducibility(seed)
-    x, y = load_dataset()
-    (train_x, train_y), (val_x, val_y), (test_x, test_y) = split_dataset(x, y, test_split_ratio, val_split_ratio)
+    classifiers = []
+    ext_hooks = (W2VExtractor, CapitalizationExtractor)
+    (train_x, train_y), (val_x, val_y), (test_x, test_y) = load_features(ext_hooks,
+                                                                         device=device,
+                                                                         valid_ratio=val_split_ratio,
+                                                                         test_ratio=test_split_ratio)
+    print(torch.max(train_x))
+    print(torch.min(train_x))
+    fc_args = {"epochs": 20, "lr": 1e-4, "batch_size": 32, "wd": 0}
+    classifiers += [("FCClassifier", lambda: classifier.FCClassifier(len(train_x[0]), dev=device), fc_args)]
 
     # ~~~~ Collect results ~~~~ #
     print("Collecting results")
-    results = {clf_name: {} for clf_name, clf in classifiers}
+    results = {clf_name: {} for clf_name, _, _ in classifiers}
     for i in range(n_runs):
         print(f"i::[{i + 1}/{n_runs}] started")
-        for clf_name, clf in classifiers:
+        for clf_name, clf_hook, extra_args in classifiers:
+            clf = clf_hook()
             print(f"Classifier: {clf_name}")
             clf.train(train_x,
-                      train_y)  # TODO should the be explicitly reinitialized (weights to be randomly picked again)?
+                      train_y,
+                      **extra_args)
             for subset_name, subset in ("train", (train_x, train_y)), ("test", (test_x, test_y)):  # ("val", val)
                 if subset_name not in results[clf_name]:
                     results[clf_name][subset_name] = {}
@@ -54,7 +63,7 @@ if __name__ == '__main__':
         print(f"i::[{i + 1}/{n_runs}] current results: {results}\n\n")
 
     metric_names = results[clf_name][subset_name][TRAITS[0]]
-    for clf_name, clf in classifiers:
+    for clf_name, clf, _ in classifiers:
         for subset_name in results[clf_name]:
             for trait_name in TRAITS:
                 for metric_name in metric_names:
@@ -75,7 +84,7 @@ if __name__ == '__main__':
     results_csv = os.path.join(save_dir, f"results_{time_str}.csv")
     with open(results_csv, 'w') as csv:
         csv.write(f"line_id,subset_name,trait_name,metric_name")
-        for clf_name, clf in classifiers:
+        for clf_name, clf, _ in classifiers:
             csv.write(f",{clf_name}-mean,{clf_name}-std")
         csv.write("\n")
         for subset_name in results[clf_name]:
@@ -83,7 +92,7 @@ if __name__ == '__main__':
                 for metric_name in metric_names:
                     line_id = f"{subset_name}-{trait_name}-{metric_name}"
                     csv.write(f"{line_id},{subset_name},{trait_name},{metric_name}")
-                    for clf_name, clf in classifiers:
+                    for clf_name, clf, _ in classifiers:
                         mean, std = results[clf_name][subset_name][trait_name][metric_name]
                         csv.write(f",{mean},{std}")
                     csv.write("\n")
